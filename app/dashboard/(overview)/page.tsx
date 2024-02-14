ea206d1ddd4bb01f6c0d345e3e9448785887833e
Pavel Petrov <denev.pavel@gmail.com>
2024-02-14 15:53:16 +0200

    chapter 16 - metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,6 +8,11 @@ import {
 } from '@/app/ui/skeletons';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title:  'Dashboard',
+};
 
 export default async function Page() {
   return (
